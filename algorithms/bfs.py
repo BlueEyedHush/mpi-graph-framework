e6d03895a305b2ff325b1f45a61e7d9896720ffb
knawara <blueeyedhush@gmail.com>
2017-03-17 10:51:43 +0100

    implemented bfs

--- algorithms/bfs.py
+++ algorithms/bfs.py
@@ -1,22 +1,63 @@
 #!/usr/bin/env python
 
 import networkx as nx
+from collections import OrderedDict
+
+class OrdSet:
+    def __init__(self):
+        self.storage = OrderedDict()
+
+    def extend(self, element_list):
+        self.storage.update(map(lambda el: (el, True), element_list))
+
+    def fifo(self):
+        el, dummy = self.storage.popitem(last=False)
+        return el
+
+    def lifo(self):
+        el, dummy = self.storage.popitem(last=True)
+        return el
+
+    def __len__(self):
+        return len(self.storage)
+
+    def __str__(self):
+        return str(self.to_list())
+
+    def to_list(self):
+        return self.storage.keys()
 
 def bfs(G, start):
-    # get vertices
-    # get neighbours from vertex
+    # same vertices are added multiple times to the vertices list
+    path = []
+    visited = []
+    start_node = G.nodes()[0]
+    vertices = OrdSet()
+    vertices.extend([start_node])
+    while vertices:
+        cv = vertices.fifo()
+        visited.append(cv)
+        path.append(cv)
+        cn = filter(lambda v: v not in visited, G.neighbors(cv))
+        print(u"Visiting {}, neighbours {}, vertices {}".format(cv, cn, vertices))
+        vertices.extend(cn)
+    return path
 
-    return list(nx.dfs_edges(G,0))
+def vertices_to_edges(vertices):
+    edges = []
+    for i in range(0, len(vertices) - 1):
+        edges.append((vertices[i], vertices[i+1]))
+    return edges
 
-def test():
+def test1():
     G = nx.complete_graph(10)
     start = 0
-    actual = bfs(G, start)
+    actual = vertices_to_edges(bfs(G, start))
     expected = list(nx.dfs_edges(G,start))
-    if(actual == expected):
+    return actual == expected
+
+if __name__ == "__main__":
+    if test1():
         print("success")
     else:
         print("failure")
-
-if __name__ == "__main__":
-    test()
