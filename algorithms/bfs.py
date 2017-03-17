7f32ae736762af7cc90a706ca7c75c1c01e37f41
knawara <blueeyedhush@gmail.com>
2017-03-17 09:33:05 +0100

    adding bfs test

--- algorithms/bfs.py
+++ algorithms/bfs.py
@@ -1,7 +1,22 @@
+#!/usr/bin/env python
 
+import networkx as nx
+
+def bfs(G, start):
+    # get vertices
+    # get neighbours from vertex
+
+    return list(nx.dfs_edges(G,0))
 
 def test():
-    pass
+    G = nx.complete_graph(10)
+    start = 0
+    actual = bfs(G, start)
+    expected = list(nx.dfs_edges(G,start))
+    if(actual == expected):
+        print("success")
+    else:
+        print("failure")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test()
