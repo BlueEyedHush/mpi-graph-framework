9b5ade4cebfa7b144f3c25fb8b8b7c942d9a97de
knawara <blueeyedhush@gmail.com>
2017-04-28 09:19:38 +0200

    [framework] using fixed-width types

--- framework/main.c
+++ framework/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <inttypes.h>
 #include <mpi.h>
 
 /* complete graph with 4 vertices */
 #define E_N 12
-unsigned short E[] = {
+uint16_t E[] = {
 		1, 2, 3,
         0, 2, 3,
         0, 1, 3,
@@ -11,7 +12,7 @@ unsigned short E[] = {
 };
 
 #define V_N 4
-unsigned short V_OFFSETS[] = {
+uint16_t V_OFFSETS[] = {
 		0,
         3,
         6,
@@ -19,11 +20,11 @@ unsigned short V_OFFSETS[] = {
 };
 
 struct neighbours {
-	unsigned short *start;
+	uint16_t *start;
 	int length;
 };
 
-struct neighbours get_neighbours(unsigned short v) {
+struct neighbours get_neighbours(uint16_t v) {
 	struct neighbours n;
 	n.start = &E[V_OFFSETS[v]];
 	n.length = (v != V_N - 1) ? (V_OFFSETS[v+1] - V_OFFSETS[v]) : (E_N - V_OFFSETS[V_N-1]);
@@ -31,11 +32,11 @@ struct neighbours get_neighbours(unsigned short v) {
 }
 
 void graph_test() {
-	for(unsigned short i = 0; i < V_N; i++) {
+	for(uint16_t i = 0; i < V_N; i++) {
 		struct neighbours n = get_neighbours(i);
-		for(unsigned short j = 0; j < n.length; j++) {
-			unsigned short n_id = n.start[j];
-			printf("%d, ", n_id);
+		for(uint16_t j = 0; j < n.length; j++) {
+			uint16_t n_id = n.start[j];
+			printf("%" SCNd16 ", ", n_id);
 		}
 		printf("\n");
 	}
@@ -53,45 +54,47 @@ int main() {
 
 	/* safety checks */
 	if (world_size != V_N) {
-		fprintf(stderr, "World size (%d) != vertex nubmer in graph (%d)\n", world_size, V_N);
+		fprintf(stderr, "World size (%" SCNd16 ") != vertex nubmer in graph (%" SCNd16 ")\n", world_size, V_N);
 		return 1;
 	}
 
 	/* gather information about rank of neighbours */
 	int wait_counter = 0;
 	struct neighbours n = get_neighbours(world_rank);
-	for(unsigned short j = 0; j < n.length; j++) {
-		unsigned short n_id = n.start[j];
+	for(uint16_t j = 0; j < n.length; j++) {
+		uint16_t n_id = n.start[j];
 		if(n_id > world_rank) {
 			wait_counter++;
 		}
 	}
-	fprintf(stderr, "[RANK %d] Waiting for %d nodes to establish colouring\n", world_rank, wait_counter);
+	fprintf(stderr, "[RANK %" SCNd16 "] Waiting for %" SCNd16 " nodes to establish colouring\n", world_rank, wait_counter);
 
 	/* wait until neighbours establish their colour */
-	unsigned short used_colours[V_N] = {0};// @todo lower upper bound can be employed
+	uint16_t used_colours[V_N] = {0};// @todo lower upper bound can be employed
 	while (wait_counter > 0) {
-		unsigned short colour = 0;
-		MPI_Recv(&colour, 1, MPI_UNSIGNED_SHORT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		uint16_t colour = 0;
+		MPI_Recv(&colour, 1, MPI_UINT16_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		fprintf(stderr, "Received colour %" SCNd16, colour);
 		used_colours[colour] = 1;
 		wait_counter--;
-		fprintf(stderr, "[RANK %d] Colour %d used by neighbour. %d still to go.\n", world_rank, colour, wait_counter);
+		fprintf(stderr, "[RANK %" SCNd16 "] Colour %" SCNd16 " used by neighbour. %" SCNd16 " still to go.\n", world_rank, colour, wait_counter);
 	}
 
 	/* choose colour */
-	int chosen_colour = -1;
-	for(unsigned short i = 0; i < V_N && chosen_colour == -1; i++) {
+	uint16_t chosen_colour = 0;
+	uint8_t colour_found = 0;
+	for(uint16_t i = 0; i < V_N && colour_found == 0; i++) {
 		if (used_colours[i] == 0) {
 			chosen_colour = i;
+			colour_found = 1;
 		}
 	}
-	fprintf(stderr, "[RANK %d] Colouring with %d\n", world_rank, chosen_colour);
+	fprintf(stderr, "[RANK %" SCNd16 "] Colouring with %" SCNd16 "\n", world_rank, chosen_colour);
 
 	/* informing neighbours with lower id */
-	for (unsigned short i = 0; i < world_rank; i++) {
-		unsigned short n_id = n.start[i];
-		unsigned short c = (unsigned short) chosen_colour;
-		MPI_Send(&c, 1, MPI_UNSIGNED_SHORT, n_id, 0, MPI_COMM_WORLD);
+	for (uint16_t i = 0; i < world_rank; i++) {
+		uint16_t n_id = n.start[i];
+		MPI_Send(&chosen_colour, 1, MPI_UINT16_T, n_id, 0, MPI_COMM_WORLD);
 	}
 
 	MPI_Barrier(MPI_COMM_WORLD);
