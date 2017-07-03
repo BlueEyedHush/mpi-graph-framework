5e88ae869ae236a470f01a72a21974392fb29aad
knawara <blueeyedhush@gmail.com>
2017-07-03 10:44:35 +0200

    [framework] generate_graph script writes to stdout instead of fixed path

--- framework/generate_graph.py
+++ framework/generate_graph.py
@@ -1,27 +1,20 @@
 
-import os
+import sys
 import networkx as nx
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+def output_adj_list(nx_graph):
+    edge_count = 0
+    neighbours_str = ""
+    for v in nx_graph.nodes():
+        ns = list(nx_graph.neighbors(v))
+        line = "{} {}\n".format(v, " ".join(map(lambda v: str(v), ns)))
+        neighbours_str += line
+        edge_count += len(ns)
 
-def build_path(gname):
-    return os.path.join(dir_path, "resources/test/", gname + ".adjl")
-
-def save_adj_list(nx_graph, name):
-    with open(build_path(name), "wbc") as f:
-
-        edge_count = 0
-        neighbours_str = ""
-        for v in nx_graph.nodes():
-            ns = list(nx_graph.neighbors(v))
-            line = "{} {}\n".format(v, " ".join(map(lambda v: str(v), ns)))
-            neighbours_str += line
-            edge_count += len(ns)
-
-        f.write("{}\n".format(nx_graph.number_of_nodes()))
-        f.write("{}\n".format(edge_count))
-        f.write(neighbours_str)
+    sys.stdout.write("{}\n".format(nx_graph.number_of_nodes()))
+    sys.stdout.write("{}\n".format(edge_count))
+    sys.stdout.write(neighbours_str)
 
 if __name__ == '__main__':
     g = nx.complete_graph(50)
-    save_adj_list(g, "complete50")
+    output_adj_list(g)
