7683451b06c40469270eec25c5b0948543353a14
knawara <blueeyedhush@gmail.com>
2017-07-03 08:22:09 +0200

    [framework] making Example test non-failing

--- framework/src/it/ExampleItTest.cpp
+++ framework/src/it/ExampleItTest.cpp
@@ -7,10 +7,5 @@ TEST(Misc, Example) {
 	int rank = -1;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	printf("\nExample integration test. Rank: %d\n", rank);
-	if (rank > 0) {
-		/* cause failure */
-		MPI_Win win;
-		MPI_Win_flush(0, win);
-	}
 	MPI_Barrier(MPI_COMM_WORLD);
 }
