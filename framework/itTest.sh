bb0bbf74b6b23e1349d0b7bffd0bfb0284d73794
knawara <blueeyedhush@gmail.com>
2017-07-03 08:00:09 +0200

    [framework] introducing facilities for integration tests of parts which require MPI

---
+++ framework/itTest.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ctest -V -R ItTests
---
+++ framework/src/it/itMain.cpp
@@ -0,0 +1,11 @@
+
+#include <gtest/gtest.h>
+#include <mpi.h>
+
+int main(int argc, char* argv[]) {
+	::testing::InitGoogleTest(&argc, argv);
+	MPI_Init(&argc, &argv);
+	int result = RUN_ALL_TESTS();
+	MPI_Finalize();
+	return result;
+}
