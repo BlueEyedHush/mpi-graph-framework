f2aa8c6f60a178cfaa7d7e3dc3103dc6e3f66bb1
knawara <blueeyedhush@gmail.com>
2017-07-14 12:38:13 +0200

    [framework] extract partitioning logic from loadPartialIntSolution

--- framework/src/main/utils/TestUtils.cpp
+++ framework/src/main/utils/TestUtils.cpp
@@ -7,19 +7,24 @@
 #include <utils/CsvReader.h>
 #include <utils/IndexPartitioner.h>
 
-int* loadPartialIntSolution(std::string solutionFilePath, int partitionCount, int partitionId) {
-	CsvReader reader(solutionFilePath);
-	std::vector<int> solution = reader.getNextLine().value();
-
-	auto p = IndexPartitioner::get_range_for_partition(solution.size(), partitionCount, partitionId);
+template<typename T>
+T* getPartition(std::vector<T> wholeSolution, int partitionCount, int partitionId) {
+	// @ToDo type conflict
+	auto p = IndexPartitioner::get_range_for_partition(wholeSolution.size(), partitionCount, partitionId);
 	int start = p.first;
 	int end = p.second;
 	int count = end-start;
 
 	int* result = new int[count];
-	memcpy(result, solution.data() + start, count*sizeof(int));
+	memcpy(result, wholeSolution.data() + start, count*sizeof(int));
 
 	return result;
+}
+
+int* loadPartialIntSolution(std::string solutionFilePath, int partitionCount, int partitionId) {
+	CsvReader reader(solutionFilePath);
+	std::vector<int> solution = reader.getNextLine().value();
+	return getPartition(solution, partitionCount, partitionId);
 }
 
 std::vector<GlobalVertexId> loadGidsFromFile(std::string path) {
