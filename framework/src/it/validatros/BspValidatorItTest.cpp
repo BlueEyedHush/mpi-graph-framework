efd2dacc73e2fe06dc090f56eaa6c9851baa7a1a
knawara <blueeyedhush@gmail.com>
2017-07-18 09:38:22 +0200

    [framework] BspValidator: added test which check if validator detects failures

--- framework/src/it/validatros/BspValidatorItTest.cpp
+++ framework/src/it/validatros/BspValidatorItTest.cpp
@@ -27,3 +27,22 @@ TEST(BspValidator, AcceptsCorrectSolutionForSTG) {
 
 	ASSERT_TRUE(validationResult);
 }
+
+TEST(BspValidator, RejectsIncorrectSolutionForSTG) {
+	int rank = -1;
+	int size = -1;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	LOG(INFO) << "Initialized MPI";
+
+	auto gp = ArrayBackedChunkedPartition::fromFile("resources/test/SimpleTestGraph.adjl", size, rank);
+	LOG(INFO) << "Loaded graph from file";
+	std::pair<GlobalVertexId, int> *ps = loadBspSolutionFromFile("resources/test/STG_incorrect.bspsol", size, rank);
+	LOG(INFO) << "Loaded solution from file";
+
+	BspValidator v;
+	bool validationResult = v.validate(&gp, ps);
+	LOG(INFO) << "Executed validator";
+
+	ASSERT_FALSE(validationResult);
+}
