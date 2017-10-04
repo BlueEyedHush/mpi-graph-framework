772ebd158d26ced2091d16346fbd4c4aba980010
knawara <blueeyedhush@gmail.com>
2017-10-04 09:50:47 +0200

    [F21] implemented simple BufferManager

---
+++ framework/src/main/utils/VariableLengthBufferManager.h
@@ -0,0 +1,35 @@
+//
+// Created by blueeyedhush on 04.10.17.
+//
+
+#ifndef FRAMEWORK_VARIABLELENGTHBUFFERMANAGER_H
+#define FRAMEWORK_VARIABLELENGTHBUFFERMANAGER_H
+
+
+#include <cstddef>
+
+template <class T> class VariableLengthBufferManager {
+public:
+	VariableLengthBufferManager() : buffer(nullptr), size(0) {}
+
+	~VariableLengthBufferManager() {
+		if(buffer != nullptr) delete[] buffer;
+	}
+
+	T* getBuffer(size_t expectedCapacity) {
+		if(buffer != nullptr) delete[] buffer;
+		size = expectedCapacity;
+		buffer = new T[expectedCapacity];
+		return buffer;
+	}
+
+	VariableLengthBufferManager(const VariableLengthBufferManager&) = delete;
+	VariableLengthBufferManager& operator=(const VariableLengthBufferManager&) = delete;
+
+private:
+	T* buffer;
+	size_t size;
+};
+
+
+#endif //FRAMEWORK_VARIABLELENGTHBUFFERMANAGER_H
