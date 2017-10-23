3242b275a45bded400fd8c600e6cc026b03f627c
knawara <blueeyedhush@gmail.com>
2017-10-24 00:08:17 +0200

    [F19] moving stuff from shared.h to Colouring.h

--- framework/src/main/algorithms/Colouring.h
+++ framework/src/main/algorithms/Colouring.h
@@ -11,4 +11,31 @@
 typedef int VertexColour;
 const MPI_Datatype VERTEX_COLOUR_MPI_TYPE = MPI_INT;
 
+namespace details {
+	const int MPI_TAG = 0;
+	const size_t OUTSTANDING_RECEIVE_REQUESTS = 5;
+
+	struct Message {
+		LocalVertexId receiving_node_id;
+		int used_colour;
+
+		static MPI_Datatype mpiDatatype() {
+			MPI_Datatype d;
+			int blocklengths[] = {1, 1};
+			MPI_Aint displacements[] = {offsetof(Message, receiving_node_id), offsetof(Message, used_colour)};
+			MPI_Datatype building_types[] = {MPI_INT, MPI_INT};
+			MPI_Type_create_struct(2, blocklengths, displacements, building_types, &d);
+
+			return d;
+		}
+	};
+
+	struct VertexTempData {
+		VertexTempData() : wait_counter(0) {}
+
+		std::set<int> used_colours;
+		int wait_counter;
+	};
+}
+
 #endif //FRAMEWORK_COLOURING_H
