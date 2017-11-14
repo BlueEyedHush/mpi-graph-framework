b63dbedb5cfcd6d78bd398228e4c68a2495cf4cf
knawara <blueeyedhush@gmail.com>
2017-11-14 16:40:12 +0100

    [F20] implementing AlgorithmAssembly

--- framework/src/main/Runner.h
+++ framework/src/main/Runner.h
@@ -29,4 +29,52 @@ AlgorithmExecutionResult runAndCheck(TGraphPartition* graph, TAlgorithm& algorit
 	return r;
 }
 
+class Assembly {
+	virtual void run() = 0;
+	virtual ~Assembly() {};
+};
+
+template <typename TGHandle, typename TAlgorithm, typename TValidator>
+class AlgorithmAssembly : Assembly {
+public:
+	AlgorithmAssembly(std::function<TGHandle&()> ghProvider,
+	                  std::function<TAlgorithm&(TGHandle&)> algorithmProvider,
+	                  std::function<TValidator&(TAlgorithm&)> validatorProvider)
+			: ghp(ghProvider), ap(algorithmProvider), vp(validatorProvider) {}
+
+	virtual void run() override {
+		TGHandle& handle = ghp();
+		auto graph = handle->getGraph();
+
+		TAlgorithm& algorithm = ap(handle);
+		bool algorithmStatus = algorithm.run(graph);
+
+		MPI_Barrier(MPI_COMM_WORLD);
+
+		auto solution = algorithm.getResult();
+
+		TValidator& validator = vp(algorithm);
+		bool validationStatus = validator.validate(graph, solution);
+
+		if (!algorithmStatus) {
+			LOG(ERROR) << "Error occured while executing algorithm";
+		} else {
+			LOG(INFO) << "Algorithm terminated successfully";
+		}
+
+		if(!validationStatus) {
+			LOG(ERROR) << "Validation failure";
+		} else {
+			LOG(INFO) << "Validation success";
+		}
+
+		handle->releaseGraph();
+	}
+
+private:
+	std::function<TGHandle&()> ghp;
+	std::function<TAlgorithm&(TGHandle&)> ap;
+	std::function<TValidator&(TAlgorithm&)> vp;
+};
+
 #endif //FRAMEWORK_RUNNER_H
