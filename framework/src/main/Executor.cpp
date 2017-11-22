e61c418a10506d1390e9662d72ab013b9041f239
knawara <blueeyedhush@gmail.com>
2017-11-22 16:57:43 +0100

    [F20] introducing Executor

---
+++ framework/src/main/Executor.cpp
@@ -0,0 +1,35 @@
+//
+// Created by blueeyedhush on 22.11.17.
+//
+
+#include <boost/program_options/variables_map.hpp>
+#include "Executor.h"
+
+Executor::Executor(std::function<void(Assembly*)> assemblyCleaner) {
+	MPI_Init(NULL, NULL);
+
+	int currentNodeId;
+	MPI_Comm_rank(MPI_COMM_WORLD, &currentNodeId);
+	int worldSize;
+	MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
+	LOG(INFO) << "NODE_ID: " << currentNodeId << " WORLD_SIZE: " << worldSize;
+}
+
+Executor::~Executor() {
+	MPI_Finalize();
+}
+
+void Executor::registerAssembly(const std::string key, Assembly* assembly) {
+	assemblies.emplace(key, assembly);
+}
+
+bool Executor::executeAssembly(const std::string key) {
+	if (assemblies.find(key) != assemblies.end()) {
+		auto* assembly = assemblies.at(key);
+		assembly->run();
+		return true;
+	} else {
+		return false;
+	}
+}
+
