d94b8ba080fa2da7d4d6e638305e45ea0f3bf456
knawara <blueeyedhush@gmail.com>
2017-11-23 12:46:26 +0100

    [F39] extracting GlobalVertexId struct used by ALHP to shared.h

---
+++ framework/src/main/representations/shared.h
@@ -0,0 +1,29 @@
+//
+// Created by blueeyedhush on 23.11.17.
+//
+
+#ifndef FRAMEWORK_SHARED_H
+#define FRAMEWORK_SHARED_H
+
+template <typename TLocalId>
+struct ALHPGlobalVertexId {
+	ALHPGlobalVertexId() : ALHPGlobalVertexId(-1, 0) {}
+	ALHPGlobalVertexId(NodeId nid, TLocalId lid) : nodeId(nid), localId(lid) {}
+
+	NodeId nodeId;
+	TLocalId localId;
+
+	static MPI_Datatype mpiDatatype() {
+		MPI_Datatype dt;
+		int blocklengths[] = {1, 1};
+		MPI_Aint displacements[] = {
+				offsetof(ALHPGlobalVertexId, nodeId),
+				offsetof(ALHPGlobalVertexId, localId)
+		};
+		MPI_Datatype building_types[] = {NODE_ID_MPI_TYPE, datatypeMap.at(typeid(TLocalId))};
+		MPI_Type_create_struct(2, blocklengths, displacements, building_types, &dt);
+		return dt;
+	}
+};
+
+#endif //FRAMEWORK_SHARED_H
