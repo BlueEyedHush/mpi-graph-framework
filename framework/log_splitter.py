690062f6973c5dde23321778abfe65f2bd8b076b
knawara <blueeyedhush@gmail.com>
2018-05-09 11:48:26 +0200

    [F47] log-splitting script cleanup

--- framework/log_splitter.py
+++ framework/log_splitter.py
@@ -2,8 +2,6 @@
 import sys
 import re
 
-READ_STDIN = True
-
 def splitter(sentence, mapping):
     split = re.split("\[[0-9]+?\]", sentence)
 
@@ -27,36 +25,31 @@ def splitter(sentence, mapping):
 
             mapping[node_id].append(str)
 
-
-
     else:
         mapping["_"].append(sentence)
 
     return mapping
 
-if READ_STDIN:
+if __name__ == "__main__":
     lines = sys.stdin.readlines()
-else:
-    f = open("ar.se")
-    lines = f.readlines()
-    f.close()
 
-node_to_log_mapping = {"_": []}
-for line in lines:
-    splitter(line, node_to_log_mapping)
+    node_to_log_mapping = {"_": []}
+    for line in lines:
+        splitter(line, node_to_log_mapping)
 
-node_to_str = {}
-for k in sorted(node_to_log_mapping.iterkeys()):
-    node_to_str[k] = "".join(node_to_log_mapping[k])
+    node_to_str = {}
+    for k in sorted(node_to_log_mapping.iterkeys()):
+        node_to_str[k] = "".join(node_to_log_mapping[k])
 
-# print to stdout everything
-for k in sorted(node_to_str.iterkeys()):
-    sys.stdout.write("~~~~ {} ~~~~\n".format(k))
-    sys.stdout.write(node_to_str[k])
-    sys.stdout.write("\n\n")
+    # print to separate files
+    for k in node_to_str.iterkeys():
+        f = open("/tmp/{}_out".format(k), "w")
+        f.write(node_to_str[k])
+        f.close()
 
-# print to separate files
-for k in node_to_str.iterkeys():
-    f = open("{}_out".format(k), "w")
-    f.write(node_to_str[k])
-    f.close()
+    # print to stdout everything
+    for k in sorted(node_to_str.iterkeys()):
+        sys.stdout.write("~~~~ {} ~~~~\n".format(k))
+        sys.stdout.write(node_to_str[k])
+        sys.stdout.write("\n\n")
+
