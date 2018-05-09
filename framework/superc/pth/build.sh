cf8a7ab87903c53bcb9cfc9f54a4e24e36d56c0b
knawara <blueeyedhush@gmail.com>
2018-05-09 20:41:55 +0200

    [F36] build - switching to GCC

--- framework/superc/pth/build.sh
+++ framework/superc/pth/build.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
+BUILD_TYPE=Release
+
 module load plgrid/tools/cmake/3.10.2
 module load tools/impi/2018.1
+module load compilers/gcc/5.3.0
 
 pushd "$HOME"/ml-graphs/  > /dev/null
 
-mkdir -p CMakeFiles
-pushd CMakeFiles  > /dev/null
+LOWERCASE_BT=`echo $BUILD_TYPE | tr '[:upper]' '[:lower]'`
+BT_SPECIFIC_DIR=cmake-build-$LOWERCASE_BT
+mkdir -p $BT_SPECIFIC_DIR
 
-cmake -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc -DCMAKE_BUILD_TYPE=Release ../
+pushd $BT_SPECIFIC_DIR  > /dev/null
+
+cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=$BUILD_TYPE ../
 if [ -z "$1" ]; then
     make all
 else
