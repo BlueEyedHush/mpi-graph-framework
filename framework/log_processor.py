d3126e376c26324eec056bf4fc783d39d428841e
knawara <blueeyedhush@gmail.com>
2018-05-10 13:07:40 +0200

    [F47] adding measurement processing to log_processor

--- framework/log_processor.py
+++ framework/log_processor.py
@@ -30,12 +30,27 @@ def splitter(sentence, mapping):
 
     return mapping
 
+def measurements_processor(node_to_log_map):
+    probes = []
+
+    for node, lines in node_to_log_map.iteritems():
+        for line in lines:
+            for m in re.finditer("\[TM:(+?):(+?)\]", line):
+                probe_name = m.group(1)
+                probe_value = m.group(2)
+
+                probes.append("[{}] {}: {}".format(node, probe_name, probe_value))
+
+    node_to_log_map["probes"] = probes
+
 if __name__ == "__main__":
     lines = sys.stdin.readlines()
 
     node_to_log_mapping = {"_": []}
     for line in lines:
         splitter(line, node_to_log_mapping)
+
+    measurements_processor(node_to_log_mapping)
 
     node_to_str = {}
     for k in sorted(node_to_log_mapping.iterkeys()):
