2f82a40972875549e64ece56e960d6b8232e4a5c
knawara <blueeyedhush@gmail.com>
2018-05-10 19:52:59 +0200

    [F47] ingrate log_processor into pipeline (so that splitting occurs automatically, without manual script invocations)

--- framework/superc/pth/t0_scheduler.py
+++ framework/superc/pth/t0_scheduler.py
@@ -5,6 +5,6 @@ from common import *
 log_dir = prepare_log_dir()
 log_prefix = os.path.join(log_dir, "framework")
 
-cmds = [framework_cli("release", "resources/main/complete50.adjl", "colouring")]
+cmds = [framework_cli("release", "resources/main/complete50.adjl", "colouring", log_dir)]
 
 os.system(run_batch_string(cmds, tasks_per_node=3, log_prefix=log_prefix))
