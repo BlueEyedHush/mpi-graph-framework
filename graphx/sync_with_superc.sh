7f20e5cc4e257afe4495f45693f80b627161f3a0
knawara <blueeyedhush@gmail.com>
2018-05-27 21:01:03 +0200

    adjusting sync script to upload only jar

--- graphx/sync_with_superc.sh
+++ graphx/sync_with_superc.sh
@@ -3,21 +3,20 @@
 REL_DIR="$(dirname "${BASH_SOURCE[0]}")"
 DIR="$(readlink -e $REL_DIR)"
 MACHINE="prometheus.cyfronet.pl"
-
-if [ ! -z "$1" ]; then
-    CUSTOM_EXCLUDE="$1"
-else
-    CUSTOM_EXCLUDE="lks73892dskfja37t2299ekk"
-fi
+LOGIN_STR="plgblueeyedhush@$MACHINE"
+REMOTE_DIR="ml-graphs/graphx/"
 
 PASS=`cat $DIR/pass`
+
+if [ ! -z "$1" ]; then
+    echo "Creating directory"
+    sshpass -p "$PASS" ssh "$LOGIN_STR" mkdir -p "$REMOTE_DIR"
+fi
+
+echo "Syncing files"
+pushd "$DIR" &> /dev/null
 sshpass -p "$PASS" rsync -avzr \
-    --exclude pass \
-    --exclude .git/ \
-    --exclude .idea/ \
-    --exclude target/ \
-    --exclude .gitignore \
-    --exclude sync_with_superc.sh \
-    --exclude "$CUSTOM_EXCLUDE" \
-    "$DIR"/. plgblueeyedhush@$MACHINE:ml-graphs/graphx
+    target/scala-2.11/*.jar \
+    "$LOGIN_STR":"$REMOTE_DIR"
+popd &> /dev/null
 
