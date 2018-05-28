446a4ff1ac01f85015f9fc746f4fa1f91d4c7351
knawara <blueeyedhush@gmail.com>
2018-05-28 09:44:48 +0200

    using 2 nodes

--- graphx/superc/pth/t0_scheduler.py
+++ graphx/superc/pth/t0_scheduler.py
@@ -7,4 +7,4 @@ log_prefix = os.path.join(log_dir, "graphx")
 
 cmds = [graphx_test_cli(log_dir)]
 
-os.system(run_batch_string(cmds, tasks_per_node=3, log_prefix=log_prefix))
+os.system(run_batch_string(cmds, node_count=2, log_prefix=log_prefix))
