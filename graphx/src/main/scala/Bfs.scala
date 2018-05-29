91183e71a07db37061464bb49dc4075a9f6695d8
knawara <blueeyedhush@gmail.com>
2018-05-29 07:30:43 +0200

    bfs - onVisit callback

--- graphx/src/main/scala/Bfs.scala
+++ graphx/src/main/scala/Bfs.scala
@@ -3,12 +3,21 @@ import org.apache.spark.graphx.{Graph, VertexId}
 
 object Bfs {
   def run[ED](graph: Graph[Boolean, ED], start: VertexId)(implicit sc: SparkContext): Unit = {
+    val onVisit: Long => Unit = vid => println(s"visisted $onVisit")
+
     graph
       .pregel(false)((vid, vdata, msg) => {
         // initial iteration
-        if (!msg) vid == start
+        if (!msg)
+          if (vid == start) {
+            onVisit(vid)
+            true
+          } else false
         // any other iteration, if we received any message it means we're visited
-        else true
+        else {
+          onVisit(vid)
+          true
+        }
       }, triplet => {
         if (triplet.srcAttr) Iterator.single((triplet.dstId, true))
         else Iterator.empty
