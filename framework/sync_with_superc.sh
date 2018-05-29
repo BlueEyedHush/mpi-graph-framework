acdb8b36037fdb70e8782ac06c689e902f70e50c
knawara <blueeyedhush@gmail.com>
2018-05-29 22:49:33 +0200

    modifing framework/sync_with_superc.sh to exclude unnecessary stuff from lib

--- framework/sync_with_superc.sh
+++ framework/sync_with_superc.sh
@@ -10,20 +10,31 @@ else
     CUSTOM_EXCLUDE="lks73892dskfja37t2299ekk"
 fi
 
-rsync -avzr \
-    --exclude pass \
+# rsync accepts first matching rule (whether it's include or exclude), so:
+# 1. exclude unwanted directories
+# 2. include all directories, since later on I want to include some stuff from lib/ (but not all!)
+# 3. exclude rest from lib/
+# 4. exclude specific files
+rsync -avzr --prune-empty-dirs \
+    --exclude "$CUSTOM_EXCLUDE" \
     --exclude CMakeFiles/ \
-    --exclude CMakeCache.txt \
     --exclude .git/ \
     --exclude .idea/ \
     --exclude Testing/ \
     --exclude target/ \
+    --include "*/" \
+    --include "*.h" \
+    --include "*.hpp" \
+    --include "*.a" \
+    --include "*.so" \
+    --exclude "lib/**" \
+    --exclude pass \
+    --exclude CMakeCache.txt \
     --exclude .gitignore \
     --exclude *.cbp \
     --exclude /*.a \
     --exclude Makefile \
     --exclude *.cmake \
     --exclude sync_with_superc.sh \
-    --exclude "$CUSTOM_EXCLUDE" \
     "$DIR"/. plgblueeyedhush@$MACHINE:ml-graphs/framework
 
