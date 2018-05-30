ccb1c1532bd5f3a1997f7cc569b64c5939215f6e
knawara <blueeyedhush@gmail.com>
2018-05-30 14:25:02 +0200

    untested implementation of colouring, part II

--- graphx/src/main/scala/perftest/Colouring.scala
+++ graphx/src/main/scala/perftest/Colouring.scala
@@ -18,61 +18,77 @@ object Colouring {
                             waitingForMe: Set[VertexId] = Set.empty,
                             coloursAlreadyUsedUp: Set[Int] = Set.empty,
                             colour: Option[Int] = None,
+                            colourPropagatedToNeighbours: Boolean = false,
                             originalData: VD)
 
   trait Message
   case class NeighbourIdAdvertisment(id: Set[VertexId]) extends Message
   case class NeighbourChoseColour(colours: Set[Int]) extends Message
+  case object ColourPropagated extends Message
   case object Dummy extends Message
 
   def run[VD: ClassTag, ED: ClassTag](graph: Graph[VD, ED])(implicit sc: SparkContext): Graph[VertexData[VD], ED] = {
     graph
       .mapVertices((_, vdata) => VertexData(originalData = vdata))
       .pregel[Message](Dummy)((vid, vdata, msg) => {
-        msg match {
-          case NeighbourIdAdvertisment(idsSet) =>
-            val (higherIdCount, lowerIdSet) = idsSet
-              .map(nVid => if (nVid > vid) (1, Set.empty) else (0, Set(nVid)))
-              .reduce { case ((c1, s1), (c2, s2)) => (c1 + c2, s1 ++ s2) }
+      msg match {
+        case NeighbourIdAdvertisment(idsSet) =>
+          val (higherIdCount, lowerIdSet) = idsSet
+            .map(nVid => if (nVid > vid) (1, Set.empty) else (0, Set(nVid)))
+            .reduce((a,b) => (a,b) match { case ((c1, s1), (c2, s2)) => (c1 + c2, s1 ++ s2) })
 
-            vdata.copy(
-              iAmWaitingFor = vdata.iAmWaitingFor + higherIdCount,
-              waitingForMe = vdata.waitingForMe ++ lowerIdSet
-            )
+          vdata.copy(
+            iAmWaitingFor = vdata.iAmWaitingFor + higherIdCount,
+            waitingForMe = vdata.waitingForMe ++ lowerIdSet
+          )
 
-          case NeighbourChoseColour(colourSet) =>
-            val stillWaitingFor = vdata.iAmWaitingFor - colourSet.size
+        case NeighbourChoseColour(colourSet) =>
+          val stillWaitingFor = vdata.iAmWaitingFor - colourSet.size
 
-            val newChosenColour =
-              if (stillWaitingFor == 0)
-               Some(1)
-              else
-               None
+          val newChosenColour =
+            if (stillWaitingFor == 0) {
+              var colour: Int = 0
+              val usedUpColoursIt = vdata.coloursAlreadyUsedUp.toSeq.sorted.iterator
+              while (usedUpColoursIt.hasNext && colour == usedUpColoursIt.next())
+                colour += 1
 
-            vdata.copy(
-              coloursAlreadyUsedUp = vdata.coloursAlreadyUsedUp ++ colourSet,
-              colour = newChosenColour
-            )
+              Some(colour)
+            } else
+              None
 
-          case Dummy => vdata
-        }
+          vdata.copy(
+            coloursAlreadyUsedUp = vdata.coloursAlreadyUsedUp ++ colourSet,
+            colour = newChosenColour
+          )
 
-      }, triplet => {
-        triplet.srcAttr match {
-          case VertexData(false, _, _, _, _, _) =>
-            Iterator.single((triplet.dstId, NeighbourIdAdvertisment(Set(triplet.srcId))))
-          case VertexData(true, _, _, _, Some(colour), _) =>
-            Iterator.single((triplet.dstId, NeighbourChoseColour(Set(colour))))
-          // todo: must put flag so that only one message is sent
-          case _ =>
-            Iterator.empty
-        }
-      }, {
-        case (Dummy, Dummy) => Dummy
-        case (NeighbourIdAdvertisment(s1), NeighbourIdAdvertisment(s2)) => NeighbourIdAdvertisment(s1 ++ s2)
-        case (NeighbourChoseColour(s1), NeighbourChoseColour(s2)) => NeighbourChoseColour(s1 ++ s2)
-        case (m1, m2) => throw new RuntimeException(s"received unexpected combination of messages:\n M1: $m1\n M2: $m2")
-      })
+        case ColourPropagated =>
+          vdata.copy(colourPropagatedToNeighbours = true)
+
+        case Dummy => vdata
+      }
+
+    }, triplet => {
+      triplet.srcAttr match {
+        case VertexData(false, _, _, _, _, _, _) =>
+          Iterator.single((triplet.dstId, NeighbourIdAdvertisment(Set(triplet.srcId))))
+        case VertexData(true, _, _, _, Some(colour), false, _) =>
+          /* we need to send message to target vertex, but also to src vertex,
+             so that src vertex can update colourPropagatedToNeighbours flag
+             (vertex program is only executed if there is any message incoming)
+          */
+          val toDest = (triplet.dstId, NeighbourChoseColour(Set(colour)))
+          val toSrc = (triplet.srcId, ColourPropagated)
+          Iterator(toDest, toSrc)
+        case _ =>
+          Iterator.empty
+      }
+    }, {
+      case (Dummy, Dummy) => Dummy
+      case (NeighbourIdAdvertisment(s1), NeighbourIdAdvertisment(s2)) => NeighbourIdAdvertisment(s1 ++ s2)
+      case (NeighbourChoseColour(s1), NeighbourChoseColour(s2)) => NeighbourChoseColour(s1 ++ s2)
+      case (ColourPropagated, ColourPropagated) => ColourPropagated
+      case (m1, m2) => throw new RuntimeException(s"received unexpected combination of messages:\n M1: $m1\n M2: $m2")
+    })
   }
 
 }
