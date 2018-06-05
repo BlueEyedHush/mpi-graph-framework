9c32e558771bf4b2cd242ee365a0971a5ca5467a
knawara <blueeyedhush@gmail.com>
2018-06-05 08:23:07 +0200

    parametrize run() by algorithm

--- graphx/src/main/scala/perftest/Main.scala
+++ graphx/src/main/scala/perftest/Main.scala
@@ -8,20 +8,37 @@ import org.apache.spark.storage.StorageLevel
 import scala.io.Source
 
 object Main {
+  object Algorithm extends Enumeration {
+    type Algorithm = Value
+    val Bfs, Colouring = Value
+  }
+
   // Start Spark.
-  def run(implicit sc: SparkContext) = {
+  def run(algo: Algorithm.Value)(implicit sc: SparkContext) = {
     // Suppress unnecessary logging.
     Logger.getRootLogger.setLevel(Level.ERROR)
 
-    val g: Graph[Boolean, Int] = Utils.loadGraphFromStdDirectory("powerlaw_25_2_05_876")
-      .mapVertices((vid, _) => false)
+    val g: Graph[Int, Int] = Utils.loadGraphFromStdDirectory("powerlaw_25_2_05_876")
 
     // Calculate centralities.
-    println("\n### Bfs\n")
-    val (startVertexId, _) = g.vertices.take(1)(0)
-    val start = System.nanoTime()
-    val visisted = Bfs.run(g, startVertexId)
-    val duration = System.nanoTime() - start
-    println(s"Bfs took $duration ns")
+
+    println(s"\n### $algo\n")
+
+    val duration = algo match {
+      case Algorithm.Bfs =>
+        val bg = g.mapVertices((vid, _) => false)
+        val (startVertexId, _) = g.vertices.take(1)(0)
+
+        val start = System.nanoTime()
+        Bfs.run(bg, startVertexId)
+        System.nanoTime() - start
+
+      case Algorithm.Colouring =>
+        val start = System.nanoTime()
+        Colouring.run(g)
+        System.nanoTime() - start
+    }
+
+    println(s"$duration ns")
   }
 }
