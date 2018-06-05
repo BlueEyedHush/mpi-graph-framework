7e065cdb592291df2ff7ae9e9c5ba8de58c99ad3
knawara <blueeyedhush@gmail.com>
2018-06-05 10:06:15 +0200

    integrate CliParser into runners

--- graphx/src/test/scala/perftest/LocalRunner.scala
+++ graphx/src/test/scala/perftest/LocalRunner.scala
@@ -1,13 +1,14 @@
 package perftest
 
 import org.apache.spark.{SparkConf, SparkContext}
+import perftest.Main.Algorithm
 
 object LocalRunner {
   def main(args: Array[String]) = {
     val sparkConf = new SparkConf().setAppName("example-graphx").setMaster("local[2]")
     implicit val sc = new SparkContext(sparkConf)
 
-    Main.run(sc)
+    Main.run(Algorithm.Colouring, 1, "../graphs/data/SimpleTestGraph.elt")
 
     sc.stop()
   }
