ec19dd546a736ec47b5e52f37e9570d23a42c9bc
knawara <blueeyedhush@gmail.com>
2018-06-05 14:25:34 +0200

    increase memory

--- graphx/superc/pth/t0_scheduler.py
+++ graphx/superc/pth/t0_scheduler.py
@@ -7,4 +7,4 @@ log_prefix = os.path.join(log_dir, "graphx")
 
 cmds = [graphx_test_cli(graph="../graphs/data/powerlaw_25_2_05_876.elt", iterations=3, algo="colouring")]
 
-os.system(run_batch_string(cmds, node_count=2, mem_per_task="2gb", log_prefix=log_prefix))
+os.system(run_batch_string(cmds, node_count=2, mem_per_task="4gb", log_prefix=log_prefix))
