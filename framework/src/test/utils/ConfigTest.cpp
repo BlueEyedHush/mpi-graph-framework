23d9ce8c46484b7bbbd4397eb85189f44d4778e0
knawara <blueeyedhush@gmail.com>
2018-06-06 21:05:31 +0200

    wrapping string in runtime_error; bugfixes: to algorithm and to test

--- framework/src/test/utils/ConfigTest.cpp
+++ framework/src/test/utils/ConfigTest.cpp
@@ -4,20 +4,20 @@
 
 TEST(ConfigLoader, CorrectlyParsesEmptyArgList) {
 	const char* argv[] = {"script"};
-	auto cm = parseCli(1, argv);
+	auto cm = parseCli(sizeof(argv)/sizeof(argv[0]), argv);
 	ASSERT_EQ(cm.size(), 0);
 }
 
 TEST(ConfigLoader, CorrectlyParsesSingleArg) {
 	const char* argv[] = {"script", "-n", "val"};
-	auto cm = parseCli(1, argv);
+	auto cm = parseCli(sizeof(argv)/sizeof(argv[0]), argv);
 	ASSERT_EQ(cm.size(), 1);
 	ASSERT_EQ(cm["n"], "val");
 }
 
 TEST(ConfigLoader, CorrectlyParsesTwoArgs) {
-	const char* argv[] = {"script", "-n", "val", "-a" "value"};
-	auto cm = parseCli(1, argv);
+	const char* argv[] = {"script", "-n", "val", "-a", "value"};
+	auto cm = parseCli(sizeof(argv)/sizeof(argv[0]), argv);
 	ASSERT_EQ(cm.size(), 2);
 	ASSERT_EQ(cm["n"], "val");
 	ASSERT_EQ(cm["a"], "value");
@@ -25,10 +25,10 @@ TEST(ConfigLoader, CorrectlyParsesTwoArgs) {
 
 TEST(ConfigLoader, FailsForEvenArgCount) {
 	const char* argv[] = {"script", "-n"};
-	ASSERT_ANY_THROW(parseCli(1, argv));
+	ASSERT_ANY_THROW(parseCli(sizeof(argv)/sizeof(argv[0]), argv));
 }
 
 TEST(ConfigLoader, FailsForIncorrectlyFormattedOption) {
 	const char* argv[] = {"script", "n", "val"};
-	ASSERT_ANY_THROW(parseCli(1, argv));
+	ASSERT_ANY_THROW(parseCli(sizeof(argv)/sizeof(argv[0]), argv));
 }
