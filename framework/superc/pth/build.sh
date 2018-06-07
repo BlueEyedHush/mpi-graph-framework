8209095628af66f8ee3580b2707aac3fec0c5dd7
knawara <blueeyedhush@gmail.com>
2018-06-07 09:26:42 +0200

    on prometheus build only framework

--- framework/superc/pth/build.sh
+++ framework/superc/pth/build.sh
@@ -20,7 +20,7 @@ if [ "$1" = "refresh" ] || [ ! -f CMakeCache.txt ]; then
     cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=$BUILD_TYPE ../
 fi
 
-make all
+make framework
 
 popd  > /dev/null
 popd  > /dev/null
