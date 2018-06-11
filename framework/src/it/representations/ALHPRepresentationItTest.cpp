ac4908edbdb8e2449933ba29ecb209219f35caa7
knawara <blueeyedhush@gmail.com>
2018-06-11 22:36:11 +0200

    ALHP ItTest also required fixing (more memory)

--- framework/src/it/representations/ALHPRepresentationItTest.cpp
+++ framework/src/it/representations/ALHPRepresentationItTest.cpp
@@ -2,25 +2,42 @@
 // Created by blueeyedhush on 14.07.17.
 //
 
+#include <utils/Config.h>
 #include <representations/AdjacencyListHashPartition.h>
 #include "RepresentationItTest.h"
 
+using GH = ALHGraphHandle<TestLocalId, TestNumId>;
 using G = ALHPGraphPartition<TestLocalId, TestNumId>;
+
+static GBAuxiliaryParams getAuxParams() {
+	ConfigMap cm;
+	cm.emplace(GH::E_DIV_OPT, "1");
+	cm.emplace(GH::V_DIV_OPT, "1");
+
+	GBAuxiliaryParams auxParams;
+	auxParams.configMap = cm;
+
+	return auxParams;
+}
 
 TEST(ALHPRepresentation, PartitioningPreservesStructureSTG) {
 
-	representationTest<ALHGraphHandle<TestLocalId, TestNumId>>(
+	representationTest<GH>(
 			[&](NodeId size, NodeId rank, auto vertexIds) {
-				return ALHGraphHandle<TestLocalId, TestNumId>("resources/test/SimpleTestGraph.adjl", vertexIds);
+				return ALHGraphHandle<TestLocalId, TestNumId>("resources/test/SimpleTestGraph.adjl",
+				                                              vertexIds,
+				                                              getAuxParams());
 			}, "resources/test/SimpleTestGraph");
 
 }
 
 TEST(ALHPRepresentation, PartitioningPreservesStructurePowerlaw0) {
 
-	representationTest<ALHGraphHandle<TestLocalId, TestNumId>>(
+	representationTest<GH>(
 			[&](NodeId size, NodeId rank, auto vertexIds) {
-				return ALHGraphHandle<TestLocalId, TestNumId>("resources/test/powerlaw_25_2_05_876.adjl", vertexIds);
+				return ALHGraphHandle<TestLocalId, TestNumId>("resources/test/powerlaw_25_2_05_876.adjl",
+				                                              vertexIds,
+				                                              getAuxParams());
 			}, "resources/test/powerlaw_25_2_05_876");
 
 }
