fafbd8f729a246467680821c5ca2a40400a3b05c
knawara <blueeyedhush@gmail.com>
2018-06-12 09:18:45 +0200

    bugfix: due to 'module load' build.py must run all commands within single shell

--- framework/superc/pth/build.py
+++ framework/superc/pth/build.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 
 import argparse
+import subprocess
+import sys
 import os
+
+def err(msg):
+    sys.stderr.write(str(msg))
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-bt', dest="build_type", default="Release", choices=["Debug", "Release"])
+    parser.add_argument('-bt', dest="build_type", default="Debug", choices=["Debug", "Release"])
     parser.add_argument('-r', dest="refresh_cmake", action="store_true", default=False)
     parser.add_argument('-v', dest="verbose", action="store_true", default=False)
 
@@ -15,27 +20,29 @@ SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 BASE_DIR = os.path.join(SCRIPT_DIR, "..", "..")
 
 args = parse_args()
-print args
+err(args)
 
-os.system(
-    "module load plgrid/tools/cmake/3.10.2"
-    "module load tools/impi/2018.1"
+cmds = [
+    "module load plgrid/tools/cmake/3.10.2",
+    "module load tools/impi/2018.1",
     "module load compilers/gcc/5.3.0"
-)
+]
 
-original_work_dir = os.path.abspath(os.getcwd())
 build_dir = os.path.join(BASE_DIR, "cmake-build-{}".format(args.build_type.lower()))
 
-os.system("mkdir -p".format(build_dir))
-os.chdir(build_dir)
+cmds += ["mkdir -p {}".format(build_dir)]
+cmds += ["pushd {} > /dev/null".format(build_dir)]
 
 if args.refresh_cmake:
-    os.system("cmake "
+    cmds += ["cmake "
               "-DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ "
-              "-DCMAKE_BUILD_TYPE={} ../".format(args.build_type))
+              "-DCMAKE_BUILD_TYPE={} ../".format(args.build_type)]
 
 verbose_str = "VERBOSE=1" if args.verbose else ""
 
-os.system("make {} framework".format(verbose_str))
+cmds += ["make {} framework".format(verbose_str)]
+cmds += ["popd > /dev/null"]
 
-os.chdir(original_work_dir)
+cmds_str = "\n".join(cmds)
+err("\n\n" + cmds_str + "\n\n")
+subprocess.call(cmds_str, shell=True)
