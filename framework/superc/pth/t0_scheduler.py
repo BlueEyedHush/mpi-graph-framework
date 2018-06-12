0551147a97285af6b8531231ec7bf656115dc083
knawara <blueeyedhush@gmail.com>
2018-06-12 21:49:40 +0200

    t0_scheduler: include mem-divs

--- framework/superc/pth/t0_scheduler.py
+++ framework/superc/pth/t0_scheduler.py
@@ -5,11 +5,15 @@ from common import *
 log_dir = prepare_log_dir()
 log_prefix = os.path.join(log_dir, "framework")
 
-cmds = [framework_cli("debug", std_g("p1k"), "colouring", log_dir)]
+nc = 2
+tpn = 3
+
+cpus = nc*tpn
+cmds = [framework_cli("debug", std_g("p10k"), "colouring", log_dir, vdiv=cpus-1, ediv=cpus-1)]
 
 os.system(run_batch_string(cmds,
                            queue="plgrid-testing",
-                           tasks_per_node=3,
-                           node_count=2,
+                           tasks_per_node=tpn,
+                           node_count=nc,
                            log_prefix=log_prefix,
                            profiling_on=True))
