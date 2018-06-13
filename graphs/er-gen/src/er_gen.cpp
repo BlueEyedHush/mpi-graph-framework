258d9ba2563aa27f6558fc60fbd99489c9477ab7
knawara <blueeyedhush@gmail.com>
2018-06-13 22:49:37 +0200

    er_gen: rename ull to more generic TVertexId

--- graphs/er-gen/src/er_gen.cpp
+++ graphs/er-gen/src/er_gen.cpp
@@ -4,30 +4,30 @@
 #include <vector>
 #include <fstream>
 
-typedef unsigned long long ull;
+typedef unsigned long long TVertexId;
 
 class BackEdgeMap {
 public:
-	void add_back_edge(ull from, ull to) { map[from].push_back(to); }
-	std::vector<ull>& get_back_edges(ull v_id) { return map[v_id]; }
-	void forget_vertex(ull v_id) { map.erase(v_id); }
+	void add_back_edge(TVertexId from, TVertexId to) { map[from].push_back(to); }
+	std::vector<TVertexId>& get_back_edges(TVertexId v_id) { return map[v_id]; }
+	void forget_vertex(TVertexId v_id) { map.erase(v_id); }
 
 private:
-	std::unordered_map<ull, std::vector<ull>> map;
+	std::unordered_map<TVertexId, std::vector<TVertexId>> map;
 };
 
-void write_neighbour(std::ofstream& f_adjl, std::ofstream& f_elt, ull src, ull dest) {
+void write_neighbour(std::ofstream& f_adjl, std::ofstream& f_elt, TVertexId src, TVertexId dest) {
 	f_adjl << src << ' ';
 	f_elt << src << '\t' << dest << '\n';
 }
 
-void g(ull v_count, double p, std::ofstream& f_adjl, std::ofstream& f_elt, unsigned int seed) {
+void g(TVertexId v_count, double p, std::ofstream& f_adjl, std::ofstream& f_elt, unsigned int seed) {
 	srand(seed);
 	int threshold = static_cast<int>(p*RAND_MAX);
 
 	BackEdgeMap be_map;
 
-	for(ull src_v_id = 0; src_v_id < v_count; src_v_id++) {
+	for(TVertexId src_v_id = 0; src_v_id < v_count; src_v_id++) {
 		/* graphs for framework are requred to: for each (u, v) in G: (v, u) in G */
 		/* to avoid duplicates, we only add edges to higher vertex ids */
 		/* we also have to remember back edges - this is handled by BackEdgeMap */
@@ -39,7 +39,7 @@ void g(ull v_count, double p, std::ofstream& f_adjl, std::ofstream& f_elt, unsig
 			write_neighbour(f_adjl, f_elt, src_v_id, dest);
 		be_map.forget_vertex(src_v_id);
 
-		for(ull dest_v_id = src_v_id + 1; dest_v_id < v_count; dest_v_id++) {
+		for(TVertexId dest_v_id = src_v_id + 1; dest_v_id < v_count; dest_v_id++) {
 			if (rand() <= threshold) {
 				/* edge chosen */
 
