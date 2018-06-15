0ec0839cc59cbf11696ad5fa9be3d723b0dcdee0
knawara <blueeyedhush@gmail.com>
2018-06-15 21:25:26 +0200

    er_gen: fixing output graph, changing model to faster one

--- graphs/er-gen/src/er_gen.cpp
+++ graphs/er-gen/src/er_gen.cpp
@@ -1,32 +1,30 @@
 
 #include <cstdlib>
 #include <unordered_map>
-#include <vector>
+#include <unordered_set>
 #include <fstream>
 #include <iostream>
+#include <random>
 
 typedef unsigned int TVertexId;
 
 class BackEdgeMap {
 public:
-	void add_back_edge(TVertexId from, TVertexId to) { map[from].push_back(to); }
-	std::vector<TVertexId>& get_back_edges(TVertexId v_id) { return map[v_id]; }
+	void add_back_edge(TVertexId from, TVertexId to) { map[from].insert(to); }
+	std::unordered_set<TVertexId>& get_back_edges(TVertexId v_id) { return map[v_id]; }
 	void forget_vertex(TVertexId v_id) { map.erase(v_id); }
 
-	std::unordered_map<TVertexId, std::vector<TVertexId>> map;
+	std::unordered_map<TVertexId, std::unordered_set<TVertexId>> map;
 };
 
-void write_neighbour(std::ofstream& f_adjl, std::ofstream& f_elt, TVertexId src, TVertexId dest) {
-	f_adjl << src << ' ';
-	f_elt << src << '\t' << dest << '\n';
-}
 
-void g(TVertexId v_count, double p, std::ofstream& f_adjl, std::ofstream& f_elt, unsigned int seed) {
-	srand(seed);
-	int threshold = static_cast<int>(p*RAND_MAX);
+void g(TVertexId v_count, size_t edges_per_vertex_soft, std::ofstream& f_adjl, std::ofstream& f_elt, std::mt19937::result_type seed)
+{
+	std::mt19937 gen{std::random_device{}()};
 
 	BackEdgeMap be_map;
 
+	size_t edge_count = 0;
 	TVertexId mask = 0xf;
 	for(TVertexId src_v_id = 0; src_v_id < v_count; src_v_id++) {
 		/* graphs for framework are requred to: for each (u, v) in G: (v, u) in G */
@@ -35,32 +33,44 @@ void g(TVertexId v_count, double p, std::ofstream& f_adjl, std::ofstream& f_elt,
 
 		f_adjl << src_v_id << ' ';
 
-		/* write cached */
-		for(auto dest: be_map.get_back_edges(src_v_id))
-			write_neighbour(f_adjl, f_elt, src_v_id, dest);
-		be_map.forget_vertex(src_v_id);
+		auto& back_edges = be_map.get_back_edges(src_v_id);
 
-		for(TVertexId dest_v_id = src_v_id + 1; dest_v_id < v_count; dest_v_id++) {
-			if (rand() <= threshold) {
-				/* edge chosen */
+		/* generate some additional edges if needed */
+		if (back_edges.size() < edges_per_vertex_soft) {
+			auto to_generate = edges_per_vertex_soft - back_edges.size();
+			to_generate *= 3;
+			to_generate /= 2;
 
-				// write to files
-				write_neighbour(f_adjl, f_elt, src_v_id, dest_v_id);
+			std::uniform_int_distribution<TVertexId> distribution(src_v_id+1, v_count-1);
+			for(size_t i = 0; i < to_generate; i++) {
+				// generate neighbour
+				TVertexId dest_v_id = distribution(gen);
+				back_edges.insert(dest_v_id);
 
 				// cache back edge
 				be_map.add_back_edge(dest_v_id, src_v_id);
 			}
 		}
 
+		/* write all neighbours */
+		for(auto dest: back_edges) {
+			f_adjl << dest << ' ';
+			f_elt << src_v_id << '\t' << dest << '\n';
+		}
+		edge_count += back_edges.size();
+		be_map.forget_vertex(src_v_id);
+
 		f_adjl << '\n';
 
 		if ((src_v_id & mask) == 0)
 			std::cout << src_v_id << "/" << v_count << '\n';
 	}
+
+	std::cout << "edge_count = " << edge_count << '\n';
 }
 
 int main(int argc, const char **argv) {
 	std::ofstream adjl("tgraph.adjl");
 	std::ofstream elt("tgraph.elt");
-	g(100000, 0.4, adjl, elt, 6537);
+	g(1000000, 10, adjl, elt, 6537);
 }
