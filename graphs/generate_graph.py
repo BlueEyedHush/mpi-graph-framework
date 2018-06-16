70159b1c28d9b367b473b1ca1496a42f7cbac0ea
knawara <blueeyedhush@gmail.com>
2018-06-16 11:46:34 +0200

    er_gen: pushing it to server (better to generate graphs on remote)

--- graphs/generate_graph.py
+++ graphs/generate_graph.py
@@ -46,12 +46,5 @@ def gen_powerlaw(vertices, edges_per_vertex):
     output_graph(fname, g)
 
 if __name__ == '__main__':
-    # gen_powerlaw(100000, 10)
-    # gen_powerlaw(10000, 10)
-    # gen_powerlaw(1000, 10)
-    # gen_powerlaw(200000, 10)
-    # gen_powerlaw(50000, 10)
-    gen_powerlaw(60000, 10)
-    gen_powerlaw(70000, 10)
-    gen_powerlaw(80000, 10)
-    gen_powerlaw(90000, 10)
+    # powergraphs generated with x10 edges
+    gen_powerlaw(500000, 10)
