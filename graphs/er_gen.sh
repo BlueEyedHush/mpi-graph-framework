4f08a3c520e446baaeed60bec1ff958e4325eae2
knawara <blueeyedhush@gmail.com>
2018-06-16 12:34:01 +0200

    er_gen.sh: adding instructions for sbatch

--- graphs/er_gen.sh
+++ graphs/er_gen.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/bash
+# SBATCH -A ccbmc6
+# SBATCH -N 1
+# SBATCH --ntasks-per-node 1
+# SBATCH --mem-per-cpu 1g
+# SBATCH --time 00:20:00
+# SBATCH -J gg
+# SBATCH -p plgrid-testing
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WD="$DIR"/data/
