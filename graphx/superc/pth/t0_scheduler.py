1e2ab9a8e19eb8ecf5e3aa58977a25428fad1d59
knawara <blueeyedhush@gmail.com>
2018-06-17 10:58:24 +0200

    graphx: different configurations scheduled via ClusterRunner instead of launch script (due to initialization time)

--- graphx/superc/pth/t0_scheduler.py
+++ graphx/superc/pth/t0_scheduler.py
@@ -5,6 +5,6 @@ from common import *
 log_dir = prepare_log_dir()
 log_prefix = os.path.join(log_dir, "graphx")
 
-cmds = [graphx_test_cli(graph="../graphs/data/powerlaw_25_2_05_876.elt", iterations=3, algo="colouring")]
+cmds = [graphx_test_cli(graph="../graphs/data/powergraph_100000_999719.elt", iterations=3, algo="colouring")]
 
-os.system(run_batch_string(cmds, node_count=2, mem_per_task="4gb", log_prefix=log_prefix))
+os.system(run_batch_string(cmds, node_count=2, mem_per_task="4gb", tasks_per_node=2, log_prefix=log_prefix, time="00:50:00"))
