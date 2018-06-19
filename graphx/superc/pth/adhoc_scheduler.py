873689bcaf8f82070fc197fac4a377143188c334
knawara <blueeyedhush@gmail.com>
2018-06-20 00:53:28 +0200

    scripts bugfix: 12-time too little memory has been assigned to executor

--- graphx/superc/pth/adhoc_scheduler.py
+++ graphx/superc/pth/adhoc_scheduler.py
@@ -9,16 +9,17 @@ g_alias, g_path = std_g("p100k")
 iter = 1
 algo = "colouring"
 
-job_name = "t0_{}_{}g_{}_{}".format(node_count*cores_per_node, mem_per_core*node_count*cores_per_node, g_alias, algo)
+mem_per_node = str(mem_per_core *cores_per_node+1) + "g"
+job_name = "t0_{}_{}_{}_{}".format(node_count, mem_per_node, g_alias, algo)
 log_dir = prepare_log_dir()
 log_prefix = os.path.join(log_dir, job_name)
 
-cmds = [graphx_test_cli(str(mem_per_core) + "g", graph=g_path, iterations=iter, algo=algo)]
+cmds = [graphx_test_cli(mem_per_node, graph=g_path, iterations=iter, algo=algo)]
 
 os.system(run_batch_string(cmds,
                            job_name=job_name,
                            node_count=node_count,
-                           mem_per_node=str(mem_per_core *cores_per_node+1) + "g",
+                           mem_per_node=mem_per_node,
                            cores_per_node=cores_per_node,
                            log_prefix=log_prefix,
                            time="00:20:00"))
