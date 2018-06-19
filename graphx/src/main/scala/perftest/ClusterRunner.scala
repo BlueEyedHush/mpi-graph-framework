f45a24ac5d11da3ab91bdd92952172c594079586
knawara <blueeyedhush@gmail.com>
2018-06-19 19:54:42 +0200

    undoing hasty changes introduced to ClusterRunner

--- graphx/src/main/scala/perftest/ClusterRunner.scala
+++ graphx/src/main/scala/perftest/ClusterRunner.scala
@@ -4,25 +4,18 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkConf, SparkContext}
 
 object ClusterRunner {
-  def stdG(name: String) = s"../graphs/data/$name.elt"
-
   def main(args: Array[String]): Unit = {
     Logger.getRootLogger.setLevel(Level.ERROR)
 
-    val sparkConf = new SparkConf().setAppName("example-graphx")
-    implicit val sc = new SparkContext(sparkConf)
-
     val cliArgs = CliParser.parseCli(args.toList)
 
-    val graphs = List(
-      stdG("powergraph_100000_999719"),
-      stdG("powergraph_200000_1999678"),
-      stdG("powergraph_300000_2999660"),
-      stdG("powergraph_400000_3999643"),
-      stdG("powergraph_500000_4999634")
-    )
+    val slurmJobId = sys.env.getOrElse("SLURM_JOB_ID", "unknownid")
+    val slurmJobName = sys.env.getOrElse("SLURM_JOB_NAME", "unknownname")
 
-    graphs.foreach(g => Main.run(cliArgs.algorithm, 1, g))
+    val sparkConf = new SparkConf().setAppName(s"${slurmJobId}_${slurmJobName}_${cliArgs.algorithm}")
+    implicit val sc = new SparkContext(sparkConf)
+
+    Main.run(cliArgs.algorithm, cliArgs.iterations, cliArgs.graphPath)
 
     sc.stop()
   }
