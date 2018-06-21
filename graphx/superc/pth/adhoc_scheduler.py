3a287a4690a83bf90800daba0188ae93f6fbd48e
knawara <blueeyedhush@gmail.com>
2018-06-21 10:40:16 +0200

    adhoc changes

--- graphx/superc/pth/adhoc_scheduler.py
+++ graphx/superc/pth/adhoc_scheduler.py
@@ -2,8 +2,8 @@
 import os
 from common import *
 
-mem_per_node = "48g"
-node_count = 1
+mem_per_node = "16g"
+node_count = 4
 cores_per_node = 12
 g_alias, g_path = std_g("p100k")
 iter = 1
@@ -12,9 +12,9 @@ algo = "colouring"
 job_name = "t0_{}_{}_{}_{}".format(node_count, mem_per_node, g_alias, algo)
 log_dir = prepare_log_dir("adhoc")
 log_prefix = os.path.join(log_dir, job_name)
-p_count = 2*node_count*cores_per_node
+p_count = 2*node_count
 
-cmds = [graphx_test_cli(mem_per_node, graph=g_path, iterations=iter, algo=algo, verbose=True, kryo=False, partition_no=p_count)]
+cmds = [graphx_test_cli(mem_per_node, graph=g_path, iterations=iter, algo=algo, kryo=False, partition_no=p_count)]
 
 os.system(run_batch_string(cmds,
                            job_name=job_name,
