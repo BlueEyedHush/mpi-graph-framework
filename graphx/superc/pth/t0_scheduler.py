cd1263bab79d9e719782e28b10b8848d761a9af2
knawara <blueeyedhush@gmail.com>
2018-06-21 10:38:47 +0200

    scripts: fixing log_dir creation

--- graphx/superc/pth/t0_scheduler.py
+++ graphx/superc/pth/t0_scheduler.py
@@ -2,18 +2,19 @@
 import os
 from common import *
 
-mem_per_node = "48g"
+mem_per_node = "16g"
 cores_per_node = 12
 partitions_per_node = 2
 g_alias, g_path = std_g("p100k")
 algo = "colouring"
 
+log_dir = prepare_log_dir("t0")
+
 def schedule_job(node_count, iter):
     job_name = "{}_{}_{}_{}_i{}".format(node_count, mem_per_node, g_alias, algo, iter)
-    log_dir = prepare_log_dir("t0")
+
     log_prefix = os.path.join(log_dir, job_name)
     p_count = partitions_per_node*node_count
-
     cmds = [graphx_test_cli(mem_per_node,
                             graph=g_path,
                             iterations=1,
